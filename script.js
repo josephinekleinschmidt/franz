c4fed7a15ed6111f1cc7c5e6cd8ddadc63e8840e
Josephine Kleinschmidt <josephinek9200@gmail.com>
2019-12-05 23:35:38 +0100

    Added boundaries + fix IOS error

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var dragon = document.querySelector('.pet__color')
 var love = 70
 
-
 displayLove()
 
 setInterval(function () {
-    love = love - 0.1
+    if (love >= 0) {
+        love = love - 0.1
+    }
     displayLove()
 }, 50);
 
@@ -14,7 +15,9 @@ function displayLove() {
 }
 
 function giveLove() {
-    love = love + 10
+    if (love <= 100) {
+        love = love + 10
+    }
     displayLove()
 }
 
